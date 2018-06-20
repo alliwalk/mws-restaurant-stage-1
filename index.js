9f823e5ae06426f4b029cd7d3f14b13adea1d853
Allison Walker <allisonw@adorama.com>
2018-06-20 19:26:23 -0400

    service worker and caching enabled, allowing caching of previously visited pages --> which allows offline first capability.

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-if ('serviceWorker' in navigator){
-  navigator.serviceWorker.register('./sw.js', {scope: './'})
+if (navigator.serviceWorker){
+  navigator.serviceWorker.register('sw.js', {scope: '/'})
     .then(function(registration){
       console.log("Service Worker is registered", registration);
     })
     .catch(function(err){
         console.log("Service Worker failed to register", err);
     })
-
 }
--- sw.js
+++ sw.js
@@ -16,51 +16,118 @@ var filesToCache = [
   'img/7.jpg',
   'img/8.jpg',
   'img/9.jpg',
-  'img/10.jpg'
+  'img/10.jpg',
+  'data/restaurants.json',
+  'index.js',
+  'sw.js'
 ];
 
 
+// How to set up a basic service worker - https://www.youtube.com/watch?v=BfL3pprhnms
+// Accessed June 20, 2018
 
 // Install SW
 self.addEventListener('install', function(event){
-  console.log("Installing Service Worker");
+  console.log("[Service Workeer] Installed ");
   event.waitUntil(
-    caches.open('files-cache-v1').then(function(cache){
-      return cache.addAll([
-        '.',
-        'index.html',
-        'restaurant.html',
-        'css/styles.css',
-        'js/dbhelper.js',
-        'js/main.js',
-        'js/restaurant.js',
-        'img/1.jpg',
-        'img/2.jpg',
-        'img/3.jpg',
-        'img/4.jpg',
-        'img/5.jpg',
-        'img/6.jpg',
-        'img/7.jpg',
-        'img/8.jpg',
-        'img/9.jpg',
-        'img/10.jpg'
-      ]);
+    caches.open(staticCacheName)
+    .then(function(cache){
+      console.log("[Service Worker] Caching");
+      return cache.addAll(filesToCache);
+    }).catch(function(err){
+      console.log("[Cache not working]", err);
     })
   );
 });
 
 // Activate serviceWorker
 self.addEventListener('activate', function(event) {
-  console.log("Activating Service Worker");
+  console.log("[Service Worker] Activated");
+
+  event.waitUntil(
+    caches.keys().then(function(cacheNames){
+      return Promise.all(cacheNames.map(function(thisCacheName){
+
+        if(thisCacheName !== staticCacheName){
+
+          console.log("[Service Worker] Removing cached files from ", thisCacheName);
+          return caches.delete(thisCacheName);
+        }
+      }))
+    })
+  )
 });
 
 
 // Fetch serviceWorker
+// self.addEventListener('fetch', function(event) {
+//   console.log("[Service Worker] Fetching", event.request.url);
+//
+//   event.respondWith(
+//
+//     caches.match(event.request).then(function (response) {
+//       if (response){
+//         console.log("[Service Worker] Found in cache", event.request.url);
+//         return response;
+//       }
+//
+//       const requestClone = event.request.clone();
+//
+//       fetch(requestClone)
+//         .then(function(response){
+//
+//           if (!response){
+//             console.log("[Service Worker] No response from fetch");
+//             return response;
+//           }
+//           const responseClone = response.clone();
+//           caches.open(staticCacheName).then(function(cache) {
+//
+//             console.log("[Service Worker] New Data New", event.request.url);
+//             cache.put(event.request, responseClone);
+//             return response;
+//           });
+//         })
+//         .catch(function(err){
+//           console.log("[Service Worker] Error fetch and caching", err);
+//         })
+//     })
+//   )
+//
+// });
+
+
+// https://developer.mozilla.org/en-US/docs/Web/API/ServiceWorkerGlobalScope
 self.addEventListener('fetch', function(event) {
-  console.log("Fetching Service Worker", event.request.url);
+  console.log('Handling fetch event for', event.request.url);
+
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      if (response) {
+        console.log('Found response in cache:', response);
+
+        return response;
+      }
+      console.log('No response found in cache. About to fetch from network...');
+
+      return fetch(event.request).then(function(response) {
+        console.log('Response from network is:', response);
+
+        //put new request into cache?
+        return caches.open(staticCacheName).then(function(cache) {
+          console.log("[Service Worker] New Data, new", event.request.url);
+          cache.add(event.request.url, response.clone());
+        });
+
+        return response;
+      }, function(error) {
+        console.error('Fetching failed:', error);
+
+        throw error;
+      });
+    })
+  );
 });
-
-
 
 // function fromCache(request) {
 //   return caches.open(staticCacheName).then(function (cache) {
