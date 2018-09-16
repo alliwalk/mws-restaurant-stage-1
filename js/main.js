7481ef4d1e934ceba6183f0d5886a66cb6ba141d
Allison Walker <allisonw@adorama.com>
2018-09-16 15:23:10 -0400

    More descriptive alt text

--- js/main.js
+++ js/main.js
@@ -160,7 +160,7 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
-  image.alt = "Image of restaurant";
+  image.alt = "Image of " + restaurant.name ;
   li.append(image);
 
   const name = document.createElement('h3');
